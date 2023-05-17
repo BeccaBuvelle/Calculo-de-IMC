8b40802be7ad00a51b99187c2be42872e414cf7f
BeccaBuvelle <126508822+BeccaBuvelle@users.noreply.github.com>
2023-05-17 12:26:24 -0300

    Add files via upload

---
+++ calculodeimc.py
@@ -0,0 +1,9 @@
+nome = 'Rebeca Dias'
+altura = 1.55
+peso = 46
+imc = (float (peso) / (altura ** altura)) 
+
+print('Rebeca Dias', nome, 'tem', altura, 'de altura.')
+print('Seu peso é', peso, 'kg, e seu imc é de', imc)
+
+#calculo simples do imc
